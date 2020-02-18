336aa4d158535942b598b75e01416c7881a04286
afeiship <1290657123@qq.com>
2020-02-18 15:34:35 +0800

    add: defa false

--- src/index.rb
+++ src/index.rb
@@ -3,25 +3,27 @@
 require "fileutils"
 require "thor"
 
-class SyncPackage < Thor
-  desc "sync DIR", "Sync for DIR"
+module ThorCli
+  class SyncPackage < Thor
+    desc "sync DIR", "Sync for DIR"
 
-  def sync(dir)
-    FileUtils.cd(dir, :verbose => true) do
-      system "ncu -u"
-      system "npm version patch --no-git-tag-version --force"
-      system "git add --all"
-      system "git commit -m 'feat: update all packages to latest'"
-      system "git push"
+    def sync(dir)
+      FileUtils.cd(dir, :verbose => true) do
+        system "ncu -u"
+        system "npm version patch --no-git-tag-version --force"
+        system "git add --all"
+        system "git commit -m 'feat: update all packages to latest'"
+        system "git push"
+      end
     end
-  end
 
-  def self.exit_on_failure?
-    0
+    def self.exit_on_failure?
+      false
+    end
   end
 end
 
-SyncPackage.start(ARGV)
+ThorCli::SyncPackage.start(ARGV)
 
 # ruby src/index.rb sync /Users/feizheng/github/next-absolute-package
 # thor install src/index.rb
